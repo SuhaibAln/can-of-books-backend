d2edc87d338ed05b9712db0f12ffbeabe90332b1
SuhaibAln <201710390@stu.meu.edu.jo>
2022-08-09 23:11:09 +0300

    lab 11

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 'use strict';
-
+// this comment is for the branch 
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose')
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 3001;
+const { bookSchema, bookModel } = require('./schemas/bookSchema');
 
+sssssssssss
+//connecting
+app.listen(PORT, () => console.log(`${PORT} Radio!!`));
+
+mongoose.connect(process.env.DB_PORT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("CONNECTION OPEN!!!")
+  })
+  .catch(err => {
+    console.log("OH NO ERROR!!!!")
+    console.log(err)
+  })
+
+
+
+//Routes
 app.get('/test', (request, response) => {
-
   response.send('test request received')
-
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.get('/books', getBooks)
+
+
+//functions
+function getBooks(req, res) {
+  bookModel.find({}, (err, data) => {
+    if (err) console.log(`error reading from the database: ${err}`);
+    else res.send(data);
+  })
+}
